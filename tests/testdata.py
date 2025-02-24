a6b45a46acb2f8613c57f11d69a883ca2aac87e4
Pavel Drovnin <pavel98rus@gmail.com>
2025-02-25 01:42:36 +0300

    testdata

---
+++ tests/testdata.py
@@ -0,0 +1,18 @@
+DEFAULT_BOAT = {
+  "name": "Титаник",
+  "seats": 2,
+  "speed": 0,
+  "direction": "Вперед"
+}
+
+PASSENGER_ONE = {
+    'name': 'Дмитрий'
+}
+
+PASSENGER_TWO = {
+    'name': 'Генадий'
+}
+
+PASSENGER_THREE = {
+    'name': 'Яна'
+}
